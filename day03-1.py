f80a534d4ff14d5618cb10130682c35204f655f3
Matthew Amundson <az8t0p@gmail.com>
2022-12-03 21:36:09 -0500

    Adding solutions for day 3

---
+++ day03-1.py
@@ -0,0 +1,44 @@
+"""
+First thought: manually build a dict or list? Sounds annoying.
+
+string.ascii_letters affords us an easy way to find the priority for a given item type.
+
+The easiest method might be simply: string.ascii_letters.index(x) + 1.
+Looks like constructing a dict might be better? O(n) vs O(1)?
+    char_dict = {}
+    for i in range(len(string.ascii_letters)):
+        char_dict[string.ascii_letters[i]] = i + 1
+- https://wiki.python.org/moin/TimeComplexity
+- https://stackoverflow.com/questions/36868479/python-str-index-time-complexity
+
+Could also potentially use ord()?
+    if x.isupper():
+        ord(x) - 38
+    else:
+        ord(x) - 96
+
+Well... the ord() solution seems simpler.
+"""
+
+def main():
+    total_priority = 0
+    with open("resources/day03-input.txt") as f:
+        for line in f:
+            if line.strip():
+                total_priority += get_priority(find_common_type(line.strip()))
+    print("Total Priority: " + str(total_priority))
+
+# Gets item type that is in both rucksack compartments
+# Is there a better way to do this?
+def find_common_type(contents):
+    length = int(len(contents) / 2)
+    comp_1 = contents[:length]
+    comp_2 = contents[length:]
+    return set(comp_1).intersection(comp_2).pop()
+
+# Return the priority value of the given item type
+def get_priority(item):
+    return ord(item) - 38 if item.isupper() else ord(item) - 96
+
+if __name__ == "__main__":
+    main()
---
+++ day03-2.py
@@ -0,0 +1,37 @@
+"""
+Build a set over every three lines.
+Use a counter to verify you're treating every three lines as a single group.
+Use the same get_priority func as last time.
+I've got an assertion just because... this seems brittle?
+
+For the first ruck in each group, we immediately turn that string into a set.
+And we intersect the remaining two strings in the group with that initial set.
+According to the instructions, that should always yield only a single common item between all three strings.
+
+The way I'm handling count feels messy, but whatever, it works.
+"""
+
+def main():
+    total_priority = 0
+    count = 0
+    ruck_set = set()
+    with open("resources/day03-input.txt") as f:
+        for line in f:
+            count += 1
+            if line.strip():
+                if not ruck_set:
+                    ruck_set = set(line.strip())
+                else:
+                    ruck_set = ruck_set.intersection(set(line.strip()))
+            if count > 2:
+                assert len(ruck_set) == 1, "More than one common item type between last three rucks"
+                total_priority += get_priority(ruck_set.pop())
+                count = 0
+    print("Total Priority: " + str(total_priority))
+
+# Return the priority value of the given item type
+def get_priority(item):
+    return ord(item) - 38 if item.isupper() else ord(item) - 96
+
+if __name__ == "__main__":
+    main()
